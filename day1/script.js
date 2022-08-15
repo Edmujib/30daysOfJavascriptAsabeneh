c0e7d981b80539edd60a300172c60e21695b35f7
abdulmujeeeb <bdulmujeebidrees@gmail.com>
2022-08-15 11:12:54 +0100

    day 1 and 2

---
+++ day1/script.js
@@ -0,0 +1,70 @@
+//1 Write a single line comment which says, comments can make code readable.
+//let firstName = 'comments can make code readable'
+
+//2 Write another single comment which says, Welcome to 30DaysOfJavaScript
+//let lastname = 'Welcome to 30DaysOfJavaScript'
+
+//3 Write a multiline comment which says, comments can make code readable, easy to reuse and informative.
+/*
+  let  multiline= 'comments can make code readable, easy to reuse and informative';
+*/
+
+//4  Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let Asabeneh = 'string, boolean, undefined, null';
+console.log(string, boolean, undefined, null);
+
+//5 Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+//6 Declare four variables without assigning values
+let firstName = ""
+console.log();
+let lastName = ""
+console.log();
+let country = ""
+console.log();
+let m = ""
+console.log();
+//7 Declare four variables with assigned values
+let dog = "barking"
+console.log(dog);
+
+let car = "driven"
+console.log(car);
+
+let house = "living"
+console.log(house);
+
+let man = "walking"
+console.log(man);
+
+//8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstname = 'Asabeneh' // first name of a person
+let lastname = 'Yetayeh' // last name of a person
+let contry = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let martial = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+
+//9 Declare variables to store your first name, last name, marital status, country and age in a single line
+let name = 'Asabeneh', job = 'teacher', live = 'Finland'
+console.log(firstName, job, live);
+
+//10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+//10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+/*I am 25 years old.
+You are 30 years old.
+*/
+let myAge = 'I am 25 years old '
+let yourAge = 'You are 30 years old'
+console.log(myAge);
+
---
+++ day2/lv1.js
@@ -0,0 +1,10 @@
+//1 
+let challenge = "30 Days Of JavaScript"
+console.log(challenge);
+//2 
+let p = "married"
+console.log(p);
+
+//3 
+//4 
+
